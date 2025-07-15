08b7bc8fbcff002916d4a32defd962f6974d8894
Abdullah <buttabdullah117@gmail.com>
2025-07-15 11:42:14 +0500

    resposive addition

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
     <div className=" bg-gray-300 w-[100%] h-[100vh] flex items-center justify-center">
-      <div className=" bg-white w-[40%] h-auto max-h-[500px] overflow-y-auto min-h-[200px] p-2">
+      <div className=" bg-white md:w-[40%] w-[100%] h-auto max-h-[500px] overflow-y-auto min-h-[200px] p-2">
         <TodoForm/>
         <Todos/>
       </div>
