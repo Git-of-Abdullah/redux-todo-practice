d1ec40d707fe58f49d6027b654c2f8417c2b945f
Abdullah <buttabdullah117@gmail.com>
2025-07-16 14:22:59 +0500

    fin

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
 
   return (
     <>
-    <div className=" bg-gray-300 w-[100%] h-[100vh] flex items-center flex-col justify-center">
-      <h1 className="text-3xl font-bold mb-3 text-black/80"> Todo App </h1>
+    <div className=" bg-[rgb(0,26,51)]/80 w-[100%] h-[100vh] flex items-center flex-col justify-center">
+      <h1 className="text-3xl font-bold mb-3 text-white"> Todo App </h1>
       <div className=" bg-white md:w-[40%] w-[100%] h-auto max-h-[500px] overflow-y-auto min-h-[200px] p-2">
         <TodoForm/>
         <Todos/>
